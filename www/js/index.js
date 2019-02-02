6548957ee8e17ae3ff5b42c8e5a4f6f641d17209
Zarar Siddiqi <zarars@gmail.com>
2019-02-02 14:45:03 -0500

    Hide zoom features.

--- www/js/index.js
+++ www/js/index.js
@@ -25,7 +25,7 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        cordova.InAppBrowser.open("https://app.raptorsrepublic.com", "_self", "location=no");
+        cordova.InAppBrowser.open("https://app.raptorsrepublic.com", "_self", "location=no,zoom=no");
     }
 };
 
